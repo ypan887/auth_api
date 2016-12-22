c47be90a793c53825843ac78c408229c2feade4a
ypan887 <ypan887@gmail.com>
2016-12-21 22:32:05 -0800

    test regular user registration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+  add_group "Controllers", "/controllers/"
+  add_group "Models", "/models/"
+  add_group "Serializers", "/serializers/"
+  add_group "Requests", "/requests/"
+  coverage_dir "public/coverage"
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -20,13 +30,15 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Request::JsonHelpers, :type => :request
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,3 +1,25 @@
 require 'rails_helper'
 
-describe 'supplies', type: :request do
+describe 'users', type: :request do
+  describe 'registered with email' do
+    let(:email){ "user@example.com" }
+    let(:password){ "password" } 
+
+    it 'should success with correct information' do
+      post "/auth", 
+        params: "\{ \"email\": \"#{email}\", \"password\": \"#{password}\" \}",
+        headers: { "Content-Type": "application/json"}
+      expect(response.status).to eq(200)
+    end
+
+    it 'should failed with incorrect information' do
+      post "/auth", 
+        params: "\{ \"email\": \"#{email}\" \}",
+        headers: { "Content-Type": "application/json"}
+      expect(response.status).to eq(422)
+    end
+  end
+
+  describe 'create through omniauth' do
+  end
+end
