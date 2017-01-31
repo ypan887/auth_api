9451af6519a99d77c90aafb266f53efbdbdd6b81
ypan887 <ypan887@gmail.com>
2017-01-30 18:07:56 -0800

    override strong parameter permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  prepend_before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    permitted_parameters = devise_parameter_sanitizer.instance_values['permitted']
+    permitted_parameters[:sign_up] << :name
+    permitted_parameters[:sign_up] << :nickname
+  end
 end
