eb34138a950d876d8f22c2fddc81a90417cb4447
ypan887 <ypan887@gmail.com>
2017-01-30 18:07:01 -0800

    add cors support

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,15 @@ module UserApi
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :delete, :put, :options],
+          max_age: 0
+      end
+    end
   end
 end
